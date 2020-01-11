54e96295f5f582264074652646afb65ab2eff405
DIJITEA <57596699+DIJITEA@users.noreply.github.com>
2020-01-11 17:14:23 +0400

    Updates

---
+++ precache-manifest.e41e5e09f55b8a6e5dba5182ee17ba93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42edfe6134889cf98c742a6c8fea0398",
+    "url": "/krt-app/index.html"
+  },
+  {
+    "revision": "2fdd71959f90fbf3fada",
+    "url": "/krt-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f9e5cbce24ddd3755e1",
+    "url": "/krt-app/static/js/2.02c3bbff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/krt-app/static/js/2.02c3bbff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fdd71959f90fbf3fada",
+    "url": "/krt-app/static/js/main.054feeaf.chunk.js"
+  },
+  {
+    "revision": "1adc5ef81c0c753aa2a4",
+    "url": "/krt-app/static/js/runtime-main.fa7f689c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/krt-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
